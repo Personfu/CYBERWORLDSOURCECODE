e30c6612483b6e1d92bd2bc1d9f53ae62085dd6a
Wednesday Addams <21669120+wednesday2024@users.noreply.github.com>
2026-03-05 03:21:19 -0600

    Last minute fixes.

--- CPI Project/Assets/Scripts/ReMix-Game/ClubPenguin/GameSettings.cs
+++ CPI Project/Assets/Scripts/ReMix-Game/ClubPenguin/GameSettings.cs
@@ -472,7 +472,7 @@ namespace ClubPenguin
 			}
 		}
 
-		[Invokable("PartySwitcher.Regular", Description = "Force regular (no annual party).")]
+		[Invokable("PartySwitcher.Evergreen", Description = "Force regular (no parties).")]
 		[PublicTweak]
 		public void AnnualParties_End()
 		{
